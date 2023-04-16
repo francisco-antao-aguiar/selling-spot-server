cf09d2850796163ddf9d4951b7862ed01fd7b345
francisco-antao-aguiar <130378547+francisco-antao-aguiar@users.noreply.github.com>
2023-04-16 23:02:11 +0100

    testing stuff

---
+++ sellingspot/src/main/java/com/sellingspot/SellingSpotApplication.java
@@ -0,0 +1,13 @@
+package com.sellingspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellingSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellingSpotApplication.class, args);
+	}
+
+}
---
+++ sellingspot/src/test/java/com/sellingspot/SellingSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellingspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellingSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
